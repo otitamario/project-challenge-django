be53662ce4d904cae11f3047625ed32e1bd59434
Mario S J <mario2013sj@gmail.com>
2022-11-16 22:15:13 -0400

    env.example

--- project_challenge/settings.py
+++ project_challenge/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv() # loads the configs from .env 
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-od1ue+$12j%%gcfqe@qw7!lx7p6qul2zaimhby!rgx_2($f)++'
+SECRET_KEY = str(os.getenv('SECRET_KEY')) 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
